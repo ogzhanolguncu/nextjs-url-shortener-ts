2b2aab6b392b76b4f0da374ec469ecffebd535c8
ogzhanolguncu <ogzhan11@gmail.com>
2023-08-01 22:03:16 +0300

    Add framer motion to animate simple stuff

--- src/components/CopyToClipboard.tsx
+++ src/components/CopyToClipboard.tsx
@@ -2,6 +2,7 @@
 
 import { ClipboardCopyIcon, CheckIcon } from "@radix-ui/react-icons";
 import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 type Props = {
   textToCopy: string | null;
@@ -27,11 +28,19 @@ export const CopyToClipboard = ({ textToCopy: redirectionPath }: Props) => {
           className="cursor-pointer rounded p-1 hover:rounded-lg "
         />
         {isSavedToClipboard && (
-          <CheckIcon
-            height="18px"
-            width="18px"
-            className="text-green-5 absolute -right-1 -top-1 rounded-md bg-emerald-300"
-          />
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <CheckIcon
+                height="18px"
+                width="18px"
+                className="text-green-5 absolute -right-1 -top-1 rounded-md bg-emerald-300"
+              />
+            </motion.div>
+          </AnimatePresence>
         )}
       </div>
       <span className="pointer-events-none absolute -bottom-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm font-medium text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100">
--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -3,6 +3,7 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { CopyToClipboard } from "./CopyToClipboard";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const Feedback = () => {
   const [redirectionPath, setRedirectionPath] = useState<string | null>(null);
@@ -15,12 +16,20 @@ export const Feedback = () => {
   }, [searchParams]);
 
   return Boolean(redirectionPath) ? (
-    <div className="md:text-md mt-5 flex w-full items-center justify-between rounded-lg bg-[#cda6f3] p-3 text-slate-800 md:mt-9">
-      <div className="flex flex-col">
-        <h2 className="mb-1 font-bold">Shortened URL:</h2>
-        <p className="font-medium">{redirectionPath}</p>
-      </div>
-      <CopyToClipboard textToCopy={redirectionPath} />
-    </div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <div className="md:text-md mt-5 flex w-full items-center justify-between rounded-lg bg-[#cda6f3] p-3 text-slate-800 md:mt-9">
+          <div className="flex flex-col">
+            <h2 className="mb-1 font-bold">Shortened URL:</h2>
+            <p className="font-medium">{redirectionPath}</p>
+          </div>
+          <CopyToClipboard textToCopy={redirectionPath} />
+        </div>
+      </motion.div>
+    </AnimatePresence>
   ) : null;
 };
