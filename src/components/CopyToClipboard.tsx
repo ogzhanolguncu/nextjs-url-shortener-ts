e6eb5aa145e134e01a98e870af7f2b7cc6240350
ogzhanolguncu <ogzhan11@gmail.com>
2023-08-01 21:39:26 +0300

    Add tooltip to notify user about copy to clipboard

---
+++ src/components/CopyToClipboard.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { ClipboardCopyIcon, CheckIcon } from "@radix-ui/react-icons";
+import React, { useState } from "react";
+
+type Props = {
+  textToCopy: string | null;
+};
+export const CopyToClipboard = ({ textToCopy: redirectionPath }: Props) => {
+  const [isSavedToClipboard, setIsSavedToClipboard] = useState(false);
+
+  const copyToClipboard = (text: string | null) => {
+    if (!text) return;
+    window.navigator.clipboard.writeText(text);
+    setIsSavedToClipboard(true);
+  };
+
+  return (
+    <div
+      className="group relative flex w-max items-center  duration-300 hover:text-slate-950"
+      onClick={() => copyToClipboard(redirectionPath)}
+    >
+      <div className="relative">
+        <ClipboardCopyIcon
+          height="40px"
+          width="40px"
+          className="cursor-pointer rounded p-1 hover:rounded-lg "
+        />
+        {isSavedToClipboard && (
+          <CheckIcon
+            height="18px"
+            width="18px"
+            className="text-green-5 absolute -right-1 -top-1 rounded-md bg-emerald-300"
+          />
+        )}
+      </div>
+      <span className="pointer-events-none absolute -bottom-7 left-0 w-max rounded bg-gray-900 px-2 py-1 text-sm font-medium text-gray-50 opacity-0 shadow transition-opacity group-hover:opacity-100">
+        {isSavedToClipboard ? "Copied" : "Copy to clipboard"}
+      </span>
+    </div>
+  );
+};
--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -2,7 +2,7 @@
 
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { ClipboardCopyIcon } from "@radix-ui/react-icons";
+import { CopyToClipboard } from "./CopyToClipboard";
 
 export const Feedback = () => {
   const [redirectionPath, setRedirectionPath] = useState<string | null>(null);
@@ -15,16 +15,12 @@ export const Feedback = () => {
   }, [searchParams]);
 
   return Boolean(redirectionPath) ? (
-    <div className="mt-5 flex w-full items-center justify-between rounded-lg bg-[#cda6f3] p-3 text-slate-800 md:mt-9 md:text-lg">
+    <div className="md:text-md mt-5 flex w-full items-center justify-between rounded-lg bg-[#cda6f3] p-3 text-slate-800 md:mt-9">
       <div className="flex flex-col">
         <h2 className="mb-1 font-bold">Shortened URL:</h2>
         <p className="font-medium">{redirectionPath}</p>
       </div>
-      <ClipboardCopyIcon
-        height="35px"
-        width="35px"
-        className="cursor-pointer rounded p-1 hover:rounded-lg hover:text-emerald-200"
-      />
+      <CopyToClipboard textToCopy={redirectionPath} />
     </div>
   ) : null;
 };
